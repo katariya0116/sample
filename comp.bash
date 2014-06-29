9ee22027df5522e3fa227dea4f535daee1f47142
katariya <katariya0116@gmail.com>
2014-06-29 22:42:41 +0900

    It is first day commit  Changes to be committed: 	new file:   comp.bash 	modified:   test.cpp

---
+++ comp.bash
@@ -0,0 +1,2 @@
+#!/bin/sh
+g++ -W -std=c++11 test.cpp -o test
--- test.cpp
+++ test.cpp
@@ -1,6 +1,46 @@
-#include <stdio.h>
+#include <memory>
+#include <iostream>
+#include <array>
+#include <algorithm>
 
-void main()
+const int map_x = 12;
+const int map_y = 21;
+const int map_block_num = map_x*map_y;
+
+constexpr char answer(int _num)
 {
-	return;
+	return static_cast<char>(static_cast<int>('a') + _num);
 }
+
+constexpr const char* map_block_chara(int _map)
+{
+	return (_map != 0 ? "* " : "  ");
+}
+
+constexpr bool check_map_wall(const int _idx, const int _map_x, const int _map_y)
+{
+	return (_idx % _map_x == 0 || _idx % _map_x == _map_x - 1 || _idx >= _map_x * (_map_y - 1));
+}
+
+int main()
+{
+	std::array<int, map_block_num> map; 
+	std::string map_disp;
+	for(int cnt = 0; cnt < map_block_num; ++cnt)
+	{
+		map[cnt] = check_map_wall(cnt, map_x, map_y) ? -1 : 0;
+	}
+	for(int cnt = 0; cnt < map_block_num; ++cnt)
+	{
+		map_disp += map_block_chara(map[cnt]);
+		if(cnt % map_x == map_x - 1){ map_disp += "\n"; }
+	}
+	std::cout << map_disp << std::endl;
+
+	auto as = 0;
+	std::cin >> as;
+
+	return 0;
+}
+
+
